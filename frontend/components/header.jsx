e3d22b66f663707a62f1d4e87edb2e847e593ce8
geno3a <vladlviv9@gmail.com>
2024-02-03 00:06:33 +0200

    test header

--- frontend/components/header.jsx
+++ frontend/components/header.jsx
@@ -1,11 +1,12 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import React from 'react'
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen'
 
 export default function Header() {
   return (
-    <View className = "border-b-2 border-neutral-400">
+    <View className = "border-b-2 border-neutral-400 justify-center">
       <Text style = {{fontSize: hp(4)}} className = "text-white mb-1">Hello, <Text className = "text-bold">Oleh</Text></Text>
+      <Image source = {require('../assets/images/portrait-white-man-isolated.jpg')} style = {{width: wp(10), height: wp(10)}} className = "rounded-full absolute self-end" />
     </View>
   )
 }
