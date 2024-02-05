e4202740999d12b30eb36ad2f7d953fd775b202a
geno3a <vladlviv9@gmail.com>
2024-02-05 19:31:04 +0200

    fix for google pixel 4

--- frontend/app/home.jsx
+++ frontend/app/home.jsx
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native'
+import { View, Text, Platform } from 'react-native'
 import React from 'react'
 import Header from '../components/header'
 import Body from '../components/body'
@@ -7,7 +7,7 @@ import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-nat
 import { useFonts, RobotoMono_400Regular } from '@expo-google-fonts/roboto-mono'
 export default function Home() {
   return (
-    <View style = {{flex: 1, width: wp(94), alignSelf: 'center'}}>
+    <View style = {{flex: 1, width: wp(94), alignSelf: 'center', marginTop: Platform.OS === 'android' ? hp(1) : hp(0)}}>
         <Header />
         <Body />
     </View>
