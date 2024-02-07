7901b47872d9eee47459e2f8c07f9f14da1dc82a
geno3a <vladlviv9@gmail.com>
2024-02-07 11:58:32 +0200

    added animation

--- frontend/app/home.jsx
+++ frontend/app/home.jsx
@@ -4,7 +4,7 @@ import Header from '../components/header'
 import Body from '../components/body'
 import Navbar from '../components/navbar'
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen'
-import { useFonts, RobotoMono_400Regular } from '@expo-google-fonts/roboto-mono'
+
 export default function Home() {
   return (
     <View style = {{flex: 1, width: wp(94), alignSelf: 'center', marginTop: Platform.OS === 'android' ? hp(1) : hp(0)}}>
--- frontend/components/flowchart.jsx
+++ frontend/components/flowchart.jsx
@@ -1,10 +1,11 @@
-import { View, Text, StyleSheet } from 'react-native'
-import React, {useState, useEffect} from 'react'
+import { View, Text } from 'react-native'
+import React, { useEffect } from 'react'
 import Svg, {Rect} from 'react-native-svg'
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen'
 import Animated, { useAnimatedProps, useSharedValue, withTiming } from 'react-native-reanimated'
 
 const AnimatedRect = Animated.createAnimatedComponent(Rect);
+
 
 export default function Flowchart() {
     const getDayOfWeek = () => {
@@ -15,40 +16,57 @@ export default function Flowchart() {
     const daysOfWeek = ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN'];
     const today = getDayOfWeek();
 
-    const steps = 14000;
+    const steps = 15020;
     const goal = 6000;
+
     const progress = Math.round((steps / goal * 100)/2.5); 
-    const progressTUE = Math.round((steps / goal * 100)/2.5) + 10;
-    const trya = progressTUE > 100 ? 97 : progressTUE;
-    console.log("progress " + progress)
-    console.log("trya "+ trya)
-    console.log("progressTUE " + progressTUE)
+    const limitedProgress = progress >= 100 ? 97 : progress;
+    console.log(limitedProgress)
+    
+
+    const fill = useSharedValue(0);
+    const linefill = useSharedValue(0);
+    useEffect(() => {
+        fill.value = withTiming(limitedProgress, {duration: 1500})
+    })
+    console.log(fill.value)
+    const animatedProps = useAnimatedProps(() => ({
+        height: `-${[fill.value]}%`
+    }))
+
+    useEffect(() => {
+        linefill.value = withTiming(wp(12), {duration: 1500})
+    })
+    const lineProps = useAnimatedProps(() => ({
+        width: linefill.value
+    }))
+
     return (
         <View className = "border-b-2 border-neutral-500" style = {{height: hp(25)}}>
         <View style = {{height: hp(20), paddingTop: hp(2)}}>
           <Svg>
             {/* Розділити на 2.5 щоб було максимум 250% */}
             {/* Monday */}
-            <Rect x = {wp(2)} y = {`${-progress + 97}%`} width = {wp(12)} height = "1.5%" fill = "#58ecdc" opacity = {1} />
-            <Rect x = {wp(2)} y = "100%" width = {wp(12)} height = {`-${progress}%`} fill = "#204444" opacity = {1} />
+            <AnimatedRect x = {wp(2)} y = {`${-limitedProgress + 97}%`} animatedProps={lineProps} height = "1.5%" fill = "#58ecdc" opacity = {1} />
+            <AnimatedRect x = {wp(2)} y = "100%" width = {wp(12)} animatedProps={animatedProps} fill = "#204444" opacity = {1} />
             {/* Tuesday */}
-            <Rect x = {wp(15)} y = {`${-trya + 97}%`} width = {wp(12)} height = "1.5%" fill = "#58ecdc" opacity = {1} />
-            <Rect x = {wp(15)} y = "100%" width = {wp(12)} height = {`-${trya}%`} fill = "#204444" opacity={1}/>
+            <AnimatedRect x = {wp(15)} y = {`${-limitedProgress + 97}%`} animatedProps={lineProps} height = "1.5%" fill = "#58ecdc" opacity = {1} />
+            <AnimatedRect x = {wp(15)} y = "100%" width = {wp(12)} animatedProps = {animatedProps} fill = "#204444" opacity={1}/>
             {/* Wednesday */}
-            <Rect x = {wp(28)} y = {`${-progress + 97}%`} width = {wp(12)} height = "1.5%" fill = "#58ecdc" opacity = {1} />
-            <Rect x = {wp(28)} y = "100%" width = {wp(12)} height = {`-${progress}%`} fill = "#204444" opacity={1}/>
+            <AnimatedRect x = {wp(28)} y = {`${-limitedProgress + 97}%`} animatedProps={lineProps} height = "1.5%" fill = "#58ecdc" opacity = {1} />
+            <AnimatedRect x = {wp(28)} y = "100%" width = {wp(12)} animatedProps={animatedProps} fill = "#204444" opacity={1}/>
             {/* Thursday */}
-            <Rect x = {wp(41)} y = {`${-progress + 97}%`} width = {wp(12)} height = "1.5%" fill = "#58ecdc" opacity = {1} />
-            <Rect x = {wp(41)} y = "100%" width = {wp(12)} height = {`-${progress}%`} fill = "#204444" opacity={1}/>
+            <AnimatedRect x = {wp(41)} y = {`${-limitedProgress + 97}%`} animatedProps={lineProps} height = "1.5%" fill = "#58ecdc" opacity = {1} />
+            <AnimatedRect x = {wp(41)} y = "100%" width = {wp(12)} animatedProps={animatedProps} fill = "#204444" opacity={1}/>
             {/* Friday */}
-            <Rect x = {wp(54)} y = {`${-progress + 97}%`} width = {wp(12)} height = "1.5%" fill = "#58ecdc" opacity = {1} />
-            <Rect x = {wp(54)} y = "100%" width = {wp(12)} height = {`-${progress}%`} fill = "#204444" opacity={1}/>
+            <AnimatedRect x = {wp(54)} y = {`${-limitedProgress + 97}%`} animatedProps={lineProps} height = "1.5%" fill = "#58ecdc" opacity = {1} />
+            <AnimatedRect x = {wp(54)} y = "100%" width = {wp(12)} animatedProps={animatedProps} fill = "#204444" opacity={1}/>
             {/* Saturday */}
-            <Rect x = {wp(67)} y = {`${-progress + 97}%`} width = {wp(12)} height = "1.5%" fill = "#58ecdc" opacity = {1} />
-            <Rect x = {wp(67)} y = "100%" width = {wp(12)} height = {`-${progress}%`} fill = "#204444" opacity={1}/>
+            <AnimatedRect x = {wp(67)} y = {`${-limitedProgress + 97}%`} animatedProps={lineProps} height = "1.5%" fill = "#58ecdc" opacity = {1} />
+            <AnimatedRect x = {wp(67)} y = "100%" width = {wp(12)} animatedProps={animatedProps} fill = "#204444" opacity={1}/>
             {/* Sunday */}
-            <Rect x = {wp(80)} y = {`${-progress + 97}%`} width = {wp(12)} height = "1.5%" fill = "#58ecdc" opacity = {1} />
-            <Rect x = {wp(80)} y = "100%" width = {wp(12)} height = {`-${progress}%`} fill = "#204444" opacity={1}/>
+            <AnimatedRect x = {wp(80)} y = {`${-limitedProgress + 97}%`} animatedProps={lineProps} height = "1.5%" fill = "#58ecdc" opacity = {1} />
+            <AnimatedRect x = {wp(80)} y = "100%" width = {wp(12)} animatedProps={animatedProps} fill = "#204444" opacity={1}/>
           </Svg>
         </View>
         <View className = "flex-row mt-1" style = {{marginLeft: wp(2)}}>
