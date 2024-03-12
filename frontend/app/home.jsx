f435debaeb06ab98cd31e8cfa2694c03eeeff8d9
Oleshka <olehit32@gmail.com>
2024-03-12 21:37:31 +0100

    Experimental backend connection to header

--- frontend/app/home.jsx
+++ frontend/app/home.jsx
@@ -1,15 +1,43 @@
 import { View, Platform } from 'react-native'
-import React from 'react'
+import {React, useState, useEffect} from 'react'
 import Header from '../components/header'
 import Body from '../components/body'
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
 export default function Home() {
+  
+  const [profile, setProfile] = useState({})
+  
+  useEffect(() => {
+    fetch('http://localhost:8000/auth/jwt/create', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({"username":"Oleh1", "password":"qwerty"})
+    }).then(response => response.json())
+    .then((data) => {
+      login(data['access'])
+    })
+  }, [])
+
+  const login = (token) => {
+    fetch('http://localhost:8000/auth/users/me/', {
+      'method': 'GET',
+      headers: {
+        'Authorization': `JWT ${token}`
+      }
+    }).then((response) => response.json())
+    .then(data => {
+      setProfile(data)
+    })
+  }
+
   return (
     <SafeAreaView style = {{flex: 1, backgroundColor: '#000'}}>
       <View style = {{flex: 1, width: wp(94), alignSelf: 'center', marginTop: Platform.OS === 'android' ? hp(1) : hp(0)}}>
-          <Header />
+          <Header name={profile.full_name} image={profile.image}/>
           <Body />
       </View>
     </SafeAreaView>
--- frontend/components/header.jsx
+++ frontend/components/header.jsx
@@ -1,15 +1,18 @@
 import { View, Text, Image } from 'react-native'
-import React from 'react'
+import {React, useEffect, useState} from 'react'
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen'
 
-export default function Header() {
+export default function Header(profile) {
+
+  console.log(profile['image'])
+  
   return (
     <View className = "border-b-2 border-neutral-500 flex-row items-center" style = {{height: hp(6.5)}}>
       <View className = 'flex-1 items-start'>
-        <Text style = {{fontSize: hp(4)}} className = "text-white mb-1">Hello, <Text className = "font-semibold">Oleh</Text></Text>
+        <Text style = {{fontSize: hp(4)}} className = "text-white mb-1">Hello, <Text className = "font-semibold">{profile['name']}</Text></Text>
       </View>
       <View className = "items-end">
-        <Image className = "rounded-full" source = {require('../assets/images/portrait-white-man-isolated.jpg')} style = {{width: hp(5), height: hp(5)}}/>
+        <Image className = "rounded-full" source = {{uri : profile['image']}} style = {{width: hp(5), height: hp(5)}}/>
       </View>
     </View>
   )  
